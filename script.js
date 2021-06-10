369acda7b75e82a885e03a6a03bee8785c4004bd
Kelly Rivas <kvrivas854@mail.cfcc.com>
2021-06-10 12:29:33 -0400

    removing unused code

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 var current = new Date();
 var date = (current.getMonth()+1) + '/' + current.getDate() + '/' + current.getFullYear();
 
-$('#cityName').submit(function(e){
+$('#cityName').submit(function(e) {
     e.preventDefault();    
     var userInput = $('#userInput').val().trim();
     var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput},us&units=imperial&APPID=3b5715c4cc3d80c499e1192a073d5982`;    
     $.ajax({
         url: queryUrl,
         method: "GET"
-    }).then(function(response){
-        console.log(response);
+    }).then(function(response) {
         var imgUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         var img = $('<img>').attr('src', imgUrl);
         var card = $('<div>').addClass('card mt-3');
